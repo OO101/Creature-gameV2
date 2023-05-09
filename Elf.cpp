74e85239a5c39c1a102b0293b09ca2f35ad18537
OO101 <119543989+OO101@users.noreply.github.com>
2023-05-08 19:38:38 -0700

    Update Elf.cpp

--- Elf.cpp
+++ Elf.cpp
@@ -1,5 +1,6 @@
 #include "Elf.h"
 #include <iostream>
+#include <string>
 using namespace std;
 namespace cs_creature{
 
@@ -13,10 +14,10 @@ int Elf::getDamage() const
 {
     int damage = Creature::getDamage();
      cout <<"The " << getSpecies() << " attacks for " << damage << " points!" << endl;
-      if((rand() % 2)== 0){
-        cout << "Magical attack inflicts " << damage << " additional damage points!" << endl;
-        damage *= 2;
-      }
+        if (rand() % 100 * 0.01 < MAGICAL_ATTACK_PROBABILITY) {
+            cout << "Magical attack inflicts " << damage << " additional damage points!" << endl;
+            damage *= MAGICAL_ATTACK_MULTIPLIER;
+        }
       return damage;
 }
-}
+};
